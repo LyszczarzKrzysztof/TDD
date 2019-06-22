26c75627a4fe16903e8f59bfcd8886ba791792b3
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2019-06-22 14:48:18 +0200

    Correct validator and test

--- src/main/java/postal/code/validator/PostalCodeValidator.java
+++ src/main/java/postal/code/validator/PostalCodeValidator.java
@@ -1,4 +1,16 @@
 package postal.code.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PostalCodeValidator {
+
+    public static final String POSTAL_CODE_REG_EX = "[0-9]{2}-[0-9]{3}";
+
+    public boolean isCorrect (String postalCode) {
+        Pattern pattern = Pattern.compile(POSTAL_CODE_REG_EX);
+        Matcher matcher = pattern.matcher(postalCode);
+
+        return matcher.matches();
+    }
 }
--- src/test/java/postal/code/validator/PostalCodeValidatorTest.java
+++ src/test/java/postal/code/validator/PostalCodeValidatorTest.java
@@ -1,7 +1,34 @@
 package postal.code.validator;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostalCodeValidatorTest {
 
+
+    @Test
+        public void isValid(){
+            PostalCodeValidator postalCodeValidator = new PostalCodeValidator();
+            assertTrue(postalCodeValidator.isCorrect("35-051"));
+            assertTrue(postalCodeValidator.isCorrect("00-459"));
+            assertTrue(postalCodeValidator.isCorrect("24-045"));
+            assertTrue(postalCodeValidator.isCorrect("60-943"));
+            assertTrue(postalCodeValidator.isCorrect("94-990"));
+    }
+
+    @Test
+    public void isNotValid(){
+        PostalCodeValidator postalCodeValidator = new PostalCodeValidator();
+        assertFalse(postalCodeValidator.isCorrect("null"));
+        assertFalse(postalCodeValidator.isCorrect(""));
+        assertFalse(postalCodeValidator.isCorrect("A9-090"));
+        assertFalse(postalCodeValidator.isCorrect("56=040"));
+        assertFalse(postalCodeValidator.isCorrect(""));
+
+
+    }
 }
