96e0def5dfd32cd2d67cbd370031563cf1a0095e
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2019-06-22 13:04:20 +0200

    GitIgnore.

--- src/test/java/edu/sample/HelloTest.java
+++ src/test/java/edu/sample/HelloTest.java
@@ -1,7 +1,14 @@
 package edu.sample;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloTest {
+
+    @Test                       // w junit 5 dziala ta anotacja i trzeba ją miec w metodach testowych - chyba
+    public void testSayHello(){
+
+    }
 
 }
