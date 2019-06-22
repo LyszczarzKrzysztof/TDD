11415577c9a5ff1c30dd360974fae30cbefb202e
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2019-06-22 15:27:42 +0200

    Correct validator and test2.

--- src/test/java/postal/code/validator/PostalCodeValidatorTest.java
+++ src/test/java/postal/code/validator/PostalCodeValidatorTest.java
@@ -2,33 +2,29 @@ package postal.code.validator;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostalCodeValidatorTest {
 
 
     @Test
-        public void isValid(){
-            PostalCodeValidator postalCodeValidator = new PostalCodeValidator();
-            assertTrue(postalCodeValidator.isCorrect("35-051"));
-            assertTrue(postalCodeValidator.isCorrect("00-459"));
-            assertTrue(postalCodeValidator.isCorrect("24-045"));
-            assertTrue(postalCodeValidator.isCorrect("60-943"));
-            assertTrue(postalCodeValidator.isCorrect("94-990"));
+    public void isValid() {
+        PostalCodeValidator postalCodeValidator = new PostalCodeValidator();
+        assertTrue(postalCodeValidator.isCorrect("00-000"));
+        assertTrue(postalCodeValidator.isCorrect("99-999"));
+        assertTrue(postalCodeValidator.isCorrect("24-045"));
+        assertTrue(postalCodeValidator.isCorrect("11-111"));
+        assertTrue(postalCodeValidator.isCorrect("94-990"));
     }
 
     @Test
-    public void isNotValid(){
+    public void isNotValid() {
         PostalCodeValidator postalCodeValidator = new PostalCodeValidator();
         assertFalse(postalCodeValidator.isCorrect("null"));
         assertFalse(postalCodeValidator.isCorrect(""));
         assertFalse(postalCodeValidator.isCorrect("A9-090"));
-        assertFalse(postalCodeValidator.isCorrect("56=040"));
-        assertFalse(postalCodeValidator.isCorrect(""));
-
-
+        assertFalse(postalCodeValidator.isCorrect("56+b10"));
+        assertFalse(postalCodeValidator.isCorrect("#+7*9"));
+        //assertFalse(postalCodeValidator.isCorrect(null));
     }
 }
